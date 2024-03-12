ffc8aee520e34c4fa78626690b0d9a26f7cc8110
marcioflaviob <mbrandao@student.42.fr>
2024-03-12 01:20:44 +0100

    fixed bugs on mac

--- client.c
+++ client.c
@@ -6,82 +6,166 @@
 /*   By: mbrandao <mbrandao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:45:34 by mbrandao          #+#    #+#             */
-/*   Updated: 2024/03/11 20:47:58 by mbrandao         ###   ########.fr       */
+/*   Updated: 2024/03/12 01:01:49 by mbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include "minitalk.h"
+
+// int	g_lock;
+
+// void	end_program(int signal)
+// {
+// 	if (signal == SIGUSR2)
+// 	{
+// 		write(1, "Message sent and signal received.\n", 34);
+// 		exit(1);
+// 	}
+// 	else
+// 		g_lock = 0;
+// }
+
+// void	send_char(int pid, unsigned char c)
+// {
+// 	int	bits;
+
+// 	g_lock = 1;
+// 	bits = 7;
+// 	while (bits >= 0)
+// 	{
+// 		if (((c >> bits) & 1) == 0)
+// 			kill(pid, SIGUSR1);
+// 		else
+// 			kill(pid, SIGUSR2);
+// 		bits--;
+// 		usleep(128);
+// 	}
+// 	while (g_lock)
+// 		usleep(128);
+// }
+
+// void	send_len(int pid, int len)
+// {
+// 	int		i;
+// 	char	*length;
+
+// 	i = 0;
+// 	length = ft_itoa(len);
+// 	while (length[i])
+// 		send_char(pid, length[i++]);
+// 	send_char(pid, '\0');
+// 	free(length);
+// }
+
+// void	send_str(int pid, char *str)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (str[i])
+// 		send_char(pid, str[i++]);
+// 	send_char(pid, '\0');
+// }
+
+// int	main(int argc, char *argv[])
+// {
+// 	int	pid;
+
+// 	if (argc != 3)
+// 	{
+// 		ft_putstr_fd("Usage: ./client PID STRING", 1);
+// 		exit(1);
+// 	}
+// 	pid = atoi(argv[1]);
+// 	signal(SIGUSR1, end_program);
+// 	signal(SIGUSR2, end_program);
+// 	// send_len(pid, ft_strlen(argv[2]));
+// 	send_str(pid, argv[2]);
+// 	while (1)
+// 		pause();
+// 	return (0);
+// }
+
+
 #include "minitalk.h"
 
-int	g_lock;
+int		g_lock = 1;
 
-void	end_program(int signal)
+void	sig_handler(int sig, siginfo_t *info, void *context)
 {
-	if (signal == SIGUSR2)
+	(void)context;
+	(void)info;
+	if (sig == SIGUSR2)
+		g_lock = 0;
+	else
 	{
 		write(1, "Message sent and signal received.\n", 34);
 		exit(1);
 	}
-	else
-		g_lock = 0;
 }
 
-void	send_char(int pid, unsigned char c)
+void	send_bit(int pid, char c, int *i)
 {
-	int	bits;
-
 	g_lock = 1;
-	bits = 7;
-	while (bits >= 0)
-	{
-		if (((c >> bits) & 1) == 0)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(256);
-		bits--;
-	}
-	while (g_lock)
-		pause();
+	if ((c >> *i) & 1)
+		kill(pid, SIGUSR1);
+	else
+		kill(pid, SIGUSR2);
+	(*i)--;
+	while (g_lock == 1)
+		usleep(100);
 }
 
-void	send_len(int pid, int len)
+void	send_null(int pid)
 {
-	int		i;
-	char	*length;
+	int	i;
 
-	i = 0;
-	length = ft_itoa(len);
-	while (length[i])
-		send_char(pid, length[i++]);
-	send_char(pid, '\0');
-	free(length);
+	i = 8;
+	while (i)
+	{
+		g_lock = 1;
+		kill(pid, SIGUSR2);
+		i--;
+		while (g_lock == 1)
+			usleep(100);
+	}
 }
 
 void	send_str(int pid, char *str)
 {
-	int	i;
+	int		i;
+	char	c;
 
-	i = 0;
-	while (str[i])
-		send_char(pid, str[i++]);
-	send_char(pid, '\0');
+	while (*str)
+	{
+		i = 7;
+		c = *str++;
+		while (i >= 0)
+			send_bit(pid, c, &i);
+	}
+	if (*str == '\0')
+	{
+		i = 8;
+		send_null(pid);
+	}
 }
 
-int	main(int argc, char *argv[])
+int	main(int argc, char **argv)
 {
-	int	pid;
+	struct sigaction	sa;
+	int					pid;
 
-	if (argc != 3)
+	if (argc != 3 || argv[2][0] == '\0')
 	{
 		ft_putstr_fd("Usage: ./client PID STRING", 1);
-		exit(1);
+		return (1);
 	}
-	pid = atoi(argv[1]);
-	signal(SIGUSR1, end_program);
-	signal(SIGUSR2, end_program);
-	// send_len(pid, ft_strlen(argv[2]));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	pid = ft_atoi(argv[1]);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	send_str(pid, argv[2]);
-	while (1)
-		pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,136 +6,189 @@
 /*   By: mbrandao <mbrandao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:45:31 by mbrandao          #+#    #+#             */
-/*   Updated: 2024/03/11 20:49:10 by mbrandao         ###   ########.fr       */
+/*   Updated: 2024/03/12 01:18:24 by mbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+// #include "minitalk.h"
 
-int	get_char(int sig)
-{
-	static int		bits;
-	static int		i;
-	static char		c;
-	char			temp;
-
-	// if (sig == SIGUSR2)
-	// 	write(1, "1", 1);
-	// else
-	// 	write(1, "0", 1);
-	if (sig == SIGUSR2)
-		c |= 1;
-	bits++;
-	if (bits == 8)
-	{
-		// write(1, ".", 1);
-		temp = c;
-		c = 0;
-		bits = 0;
-		i = 0;
-		return (temp);
-	}
-	else
-		c <<= 1;
-	return (-1);
-}
-
-// char	*str_builder(int sig, int len, siginfo_t *info)
-void	sig_handler(int sig, siginfo_t *info, void *context)
-{
-	static char	*str;
-	char		c;
-
-	(void) context;
-	c = get_char(sig);
-	if (c == -1)
-	{
-		kill(info->si_pid, SIGUSR1);
-		// write(1, "here", 4);
-		return ;
-	}
-	else if (c == '\0')
-	{
-		ft_putstr_fd(str, 1);
-		free(str);
-		str = NULL;
-		kill(info->si_pid, SIGUSR2);
-	}
-	else
-	{
-		// write(1, "aaa", 3);
-		// write(1, &c, 1);
-		str = ft_strjoin(str, &c);
-		kill(info->si_pid, SIGUSR1);
-	}
-}
-
-// int	len_calc(int sig)
+// int	get_char(int sig)
 // {
-// 	static int	c;
-// 	static int	i;
-// 	static char	*len;
-// 	int			temp;
+// 	static int		bits;
+// 	static int		i;
+// 	static char		c;
+// 	char			temp;
 
-// 	if (!len)
+// 	// if (sig == SIGUSR2)
+// 	// 	write(1, "1", 1);
+// 	// else
+// 	// 	write(1, "0", 1);
+// 	if (sig == SIGUSR2)
+// 		c |= 1;
+// 	bits++;
+// 	if (bits == 8)
 // 	{
-// 		len = (char *) ft_calloc(11, sizeof(char));
-// 		if (!len)
-// 			return (0);
+// 		// write(1, ".", 1);
+// 		temp = c;
+// 		c = 0;
+// 		bits = 0;
+// 		i = 0;
+// 		return (temp);
 // 	}
-// 	c = get_char(sig);
-// 	while (c == -1)
-// 		return (0);
-// 	while (c != '\0')
-// 	{
-// 		len[i++] = (char) c;
-// 		return (0);
-// 	}
-// 	len[i] = 0;
-// 	i = 0;
-// 	temp = ft_atoi(len);
-// 	free(len);
-// 	len = NULL;
-// 	return (temp);
+// 	else
+// 		c <<= 1;
+// 	return (-1);
 // }
 
+// // char	*str_builder(int sig, int len, siginfo_t *info)
 // void	sig_handler(int sig, siginfo_t *info, void *context)
 // {
-// 	static int	len;
-// 	char		*str;
+// 	static char	*str;
+// 	char		c;
 
 // 	(void) context;
-// 	if (!len)
+// 	c = get_char(sig);
+// 	if (c == -1)
 // 	{
-// 		len = len_calc(sig, info);
-// 		if (!len)
-// 			return ;
+// 		kill(info->si_pid, SIGUSR1);
+// 		// write(1, "here", 4);
 // 		return ;
 // 	}
-// 	str = str_builder(sig, len, info);
-// 	if (!str)
-// 		return ;
-// 	len = 0;
-// 	ft_putstr_fd(str, 1);
-// 	free(str);
-// 	str = NULL;
-// 	kill(info->si_pid, SIGUSR2);
+// 	else if (c == '\0')
+// 	{
+// 		ft_putstr_fd(str, 1);
+// 		free(str);
+// 		str = NULL;
+// 		kill(info->si_pid, SIGUSR2);
+// 	}
+// 	else
+// 	{
+// 		// write(1, "aaa", 3);
+// 		// write(1, &c, 1);
+// 		str = ft_strjoin(str, &c);
+// 		kill(info->si_pid, SIGUSR1);
+// 	}
 // }
+
+// // int	len_calc(int sig)
+// // {
+// // 	static int	c;
+// // 	static int	i;
+// // 	static char	*len;
+// // 	int			temp;
+
+// // 	if (!len)
+// // 	{
+// // 		len = (char *) ft_calloc(11, sizeof(char));
+// // 		if (!len)
+// // 			return (0);
+// // 	}
+// // 	c = get_char(sig);
+// // 	while (c == -1)
+// // 		return (0);
+// // 	while (c != '\0')
+// // 	{
+// // 		len[i++] = (char) c;
+// // 		return (0);
+// // 	}
+// // 	len[i] = 0;
+// // 	i = 0;
+// // 	temp = ft_atoi(len);
+// // 	free(len);
+// // 	len = NULL;
+// // 	return (temp);
+// // }
+
+// // void	sig_handler(int sig, siginfo_t *info, void *context)
+// // {
+// // 	static int	len;
+// // 	char		*str;
+
+// // 	(void) context;
+// // 	if (!len)
+// // 	{
+// // 		len = len_calc(sig, info);
+// // 		if (!len)
+// // 			return ;
+// // 		return ;
+// // 	}
+// // 	str = str_builder(sig, len, info);
+// // 	if (!str)
+// // 		return ;
+// // 	len = 0;
+// // 	ft_putstr_fd(str, 1);
+// // 	free(str);
+// // 	str = NULL;
+// // 	kill(info->si_pid, SIGUSR2);
+// // }
+
+// int	main(void)
+// {
+// 	struct sigaction	s_sigaction;
+
+// 	ft_putstr_fd("PID: ", 1);
+// 	ft_putnbr_fd(getpid(), 1);
+// 	ft_putchar_fd('\n', 1);
+// 	s_sigaction.sa_sigaction = sig_handler;
+// 	sigemptyset(&s_sigaction.sa_mask);
+// 	sigaddset(&s_sigaction.sa_mask, SIGUSR1);
+// 	sigaddset(&s_sigaction.sa_mask, SIGUSR2);
+// 	s_sigaction.sa_flags = SA_SIGINFO;
+// 	sigaction(SIGUSR1, &s_sigaction, 0);
+// 	sigaction(SIGUSR2, &s_sigaction, 0);
+// 	while (1)
+// 		pause();
+// 	return (0);
+// }
+
+#include "minitalk.h"
+
+void	set_bit(int signum, char *c)
+{
+	*c <<= 1;
+	if (signum == SIGUSR1)
+		*c |= 1;
+}
+
+void	free_and_print(char **str, int *i, siginfo_t *info)
+{
+	ft_putstr_fd(*str, 1);
+	free(*str);
+	*str = NULL;
+	*i = 0;
+	kill(info->si_pid, SIGUSR1);
+}
+
+void	sig_handler(int signum, siginfo_t *info, void *context)
+{
+	static char	c;
+	static char	*str;
+	static int	i;
+
+	(void)context;
+	set_bit(signum, &c);
+	if (++i == 8)
+	{
+		i = 0;
+		if (c == '\0')
+			free_and_print(&str, &i, info);
+		str = ft_strjoin(str, &c);
+	}
+	kill(info->si_pid, SIGUSR2);
+}
 
 int	main(void)
 {
-	struct sigaction	s_sigaction;
+	struct sigaction	sa;
 
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
 	ft_putstr_fd("PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
-	s_sigaction.sa_sigaction = sig_handler;
-	sigemptyset(&s_sigaction.sa_mask);
-	sigaddset(&s_sigaction.sa_mask, SIGUSR1);
-	sigaddset(&s_sigaction.sa_mask, SIGUSR2);
-	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 	return (0);
